79dc22158c4a5a6bf831a7762161029b47000777
rune0018 <rune00nielsen@gmail.com>
2021-10-06 12:42:50 +0200

    manglede noget

---
+++ Costumor/Costumors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPsystem
+{
+    class Costumors
+    {
+        public static List<Costumor> costumors { get; set; }
+    }
+}
